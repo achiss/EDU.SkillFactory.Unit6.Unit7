e4211d3b4c769b7d31bf9d07e2207857e244f5b6
S-Achi-S <irosru@gmail.com>
2023-09-01 18:07:38 +0300

    add non exercise classes

--- FinalExercise/Program.cs
+++ FinalExercise/Program.cs
@@ -4,7 +4,7 @@ namespace FinalExercise
     /*
     Абстрактный класс "User". 
     На его основе описываются все пользователи системы.
-    Сотрудник "Employee";
+    Сотрудник "Employee" (только сотрудники отдела доставки);
     Покупатель "Customer":
        - незарегистрированный покупатель ;
        - зарегистрированный покупатель.
@@ -13,14 +13,89 @@ namespace FinalExercise
     */
     abstract class User
     {
-        protected bool isUserType;
-        protected string ID;
-        public string Name;
-        public string Email;
+        protected bool isUserType;      // Если правда то покупатель, в противном случае сотрудник
+        public string ID;
+        public string Name;             // Заполнятся вручную
+        public string Surname;          // Заполнятся вручную
+        public string Email;            // Заполнятся вручную
 
-        public void GenerateIdentificationNumber(out string ID)
+        public User(string id, string name, string surname, string email)
         {
-            
+            ID = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+
+        public void GenerateIdentificationNumber(in bool isUserType, out string ID)
+        {
+            Random value = new Random();
+            int number = value.Next(000001, 999999);
+            if (isUserType)
+            {
+                string userType = "Customer_";
+                ID = string.Format("{0}{1}", userType, number.ToString());
+            }
+            else
+            {
+                string userType = "Employee_";
+                ID = string.Format("{0}{1}", userType, number.ToString());
+            }
+        }
+    }
+
+    class Customer : User
+    {
+        public string Phone;             // Заполнятся вручную
+        public string City;              // Заполнятся вручную
+        public string Adress;            // Заполнятся вручную
+        
+        public Customer(string id, string name, string surname, string email, string phone, string city, string adress) 
+            : base(id, name, surname, email)
+        {
+            Phone = phone;
+            City = city;
+            Adress = adress;
+        }
+    }
+
+    class UnregisteredCustomer : Customer
+    {
+        
+    }
+
+    class RegisteredCustomer : Customer
+    {
+        
+    }
+
+    class Employee: User
+    {
+        public bool isOnShift;          // Работает ли сотрудник
+
+        /*
+        enum Area
+        {
+            None,
+            North,
+            South,
+            East,
+            West
+        }
+
+        enum Role
+        {
+            Logistician,
+            Courier,
+            OfficeManager,
+            OfficeWorker
+        }
+        */
+
+        public Employee(string id, string name, string surname, string email, bool isOnShift) : base(id, name, surname,
+            email)
+        {
+            isOnShift = isOnShift;
         }
     }
 }
