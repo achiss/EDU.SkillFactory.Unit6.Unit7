c5fe8602187a37df45d861fa48d299cca9799b0b
S-Achi-S <irosru@gmail.com>
2023-09-01 17:08:12 +0300

    first class

--- FinalExercise/Program.cs
+++ FinalExercise/Program.cs
@@ -1,72 +1,26 @@
 ﻿
-namespace FinalExercise                         //
+namespace FinalExercise                        
 {
-    abstract class Customer
+    /*
+    Абстрактный класс "User". 
+    На его основе описываются все пользователи системы.
+    Сотрудник "Employee";
+    Покупатель "Customer":
+       - незарегистрированный покупатель ;
+       - зарегистрированный покупатель.
+       
+    П.С. поле "пароль" опущено!  
+    */
+    abstract class User
     {
+        protected bool isUserType;
+        protected string ID;
         public string Name;
-        public string Surname;
-        public int Age;
         public string Email;
-        
-        enum DiscontType            // Выбираем шаг скидки
-        {
-            None = 0,
-            TenPercent,
-            FifteenPercent
-        }
 
-        enum DeliveryType           // Задаем тип доставки
+        public void GenerateIdentificationNumber(out string ID)
         {
-            None,
-            Pickup,
-            shopDelivery,
-            homeDelivery
+            
         }
     }
-
-    class RegisteredCustomer: Customer
-    {
-        public string phoneNumber;
-        protected string homeAddress;
-        protected decimal moneySpend;
-        protected string registrationDate;
-    }
-    
-    abstract class Delivery 
-    {
-        public string Address;
-    }
-
-    class HomeDelivery: Delivery 
-    {
-        /* ... */
-    }
-
-    class PickPointDelivery: Delivery 
-    {
-        /* ... */
-    }
-
-    class ShopDelivery: Delivery 
-    {
-        /* ... */
-    }
-
-    class Order < TDelivery,
-        TStruct > where TDelivery: Delivery 
-    {
-        public TDelivery Delivery;
-
-        public int orderNumber;
-
-        public string Description;
-
-        public void DisplayAddress() 
-        {
-            Console.WriteLine(Delivery.Address);
-        }
-
-        // ... Другие поля
-    }
-    
 }
