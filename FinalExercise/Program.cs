0891a940f3576949b54a6b29f0d35d352ee330cc
sachis <135745443+S-Achi-S@users.noreply.github.com>
2023-09-05 23:11:14 +0300

    Add something

--- FinalExercise/Program.cs
+++ FinalExercise/Program.cs
@@ -2,29 +2,64 @@
 {
     abstract class Person
     {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public DateTime Birhday { get; set; }
-        public string City { get; set; }
-        public int Age
+        private string Name { get; set; }
+
+        public string Phone
         {
-            get {  ; }
-            set {  ; }
+            get
+            {
+                return Phone;
+            }
+            set
+            {
+
+                if ((Phone.Length != 11) && (Phone.Contains("7")))
+                {
+                    Phone = value;
+                }
+            }
+        }
+        public string City { get; set; }
+        public byte Age
+        {
+            get
+            {
+                return Age;
+            }
+            set
+            {
+                if (Age > 0)
+                {
+                    Age = value;
+                }
+            }
         }
 
-        protected Person(string name, DateTime birhday, string phone, string city)
+        protected Person(string name, byte age, string phone, string city)
         {
             Name = name;
             Phone = phone;
-            Birhday = birhday;
+            Age = age;
             City = city;
         }
 
         public void Greetings() => Console.WriteLine($"Hello {Name}");
+        public abstract void ShowCity();
+    }
 
-        public void AgeDetermination(in DateTime Birhday, out int Age)
+    class Employee : Person
+    {
+        protected string employeeName;
+        public string name
+        {
+            get { return employeeName; }
+            set { employeeName = value; }
+        }
+
+        private Employee() :
+            base(name, age, phone, city)
         {
             
-        } 
+        }
     }
 }
